f21b2f79e57ecd730a2d32156638515e6cd1df99
Francois A <francoisancel@gmail.com>
2016-12-21 23:47:43 +0100

    cleaner return_status structure

--- return_status.hpp
+++ return_status.hpp
@@ -10,33 +10,35 @@ namespace xts
    struct basic_return_status
    {
       typedef bool(*test_status_function)(const STATUS_TYPE&);
-
+	  typedef STATUS_TYPE status_t;
+	  typedef RETURN_TYPE return_t;
+   
    private:
-      STATUS_TYPE _status;
-      RETURN_TYPE rtype;
-      test_status_function status_test;
+	  status_t stat = status_t{};
+      return_t rtype = return_t{};
+      test_status_function status_test = nullptr;
 
    public:
-      basic_return_status() : _status(), rtype(), status_test(nullptr) {}
+	   basic_return_status() = default;
       basic_return_status(const basic_return_status&) = default;
-      basic_return_status(basic_return_status&&) = default;
+      basic_return_status(basic_return_status&&) noexcept = default;
       ~basic_return_status() = default;
 
       basic_return_status& operator=(const basic_return_status&) = default;
-      basic_return_status& operator=(basic_return_status&&) = default;
+      basic_return_status& operator=(basic_return_status&&) noexcept = default;
 
       basic_return_status(const STATUS_TYPE& st, const RETURN_TYPE& rt, test_status_function status_test)
-         : _status(st), rtype(rt), status_test(status_test) {}
+         : stat(st), rtype(rt), status_test(status_test) {}
 
       basic_return_status(const STATUS_TYPE& st, RETURN_TYPE&& rt, test_status_function status_test)
-         : _status(st), rtype(rt), status_test(status_test) {}
+         : stat(st), rtype(rt), status_test(status_test) {}
 
       operator bool() const { return valid(); }
       operator STATUS_TYPE() const { return status(); }
-      bool valid() const { return status_test ? status_test(_status) : false; }
+      bool valid() const { return status_test ? status_test(stat) : false; }
 
-      const STATUS_TYPE& status() const { return _status; }
-      STATUS_TYPE& status() { return _status; }
+      const STATUS_TYPE& status() const { return stat; }
+      STATUS_TYPE& status() { return stat; }
       const RETURN_TYPE& value() const { return rtype; }
       RETURN_TYPE& value() { return rtype; }
    };
@@ -56,16 +58,16 @@ namespace xts
          : return_status(EXIT_FAILURE)
       {}
 
-      return_status(int sta)
+      return_status(status_t sta)
          : return_status(sta, RETURN_TYPE())
       {}
 
-      return_status(int sta, const RETURN_TYPE& rt)
-         : basic_return_status<int, RETURN_TYPE>(sta, rt, [](const int& st) { return st == EXIT_SUCCESS; })
-      {}
+      //return_status(status_t sta, const RETURN_TYPE& rt)
+      //   : basic_return_status<int, RETURN_TYPE>(sta, rt, [](const status_t& st) { return st == EXIT_SUCCESS; })
+      //{}
 
-      return_status(int sta, RETURN_TYPE&& rt)
-         : basic_return_status<int, RETURN_TYPE>(sta, rt, [](const int& st) { return st == EXIT_SUCCESS; })
+      return_status(status_t sta, RETURN_TYPE&& rt)
+         : basic_return_status<int, RETURN_TYPE>(sta, rt, [](const status_t& st) { return st == EXIT_SUCCESS; })
       {}
 
       return_status(const return_status&) = default;
