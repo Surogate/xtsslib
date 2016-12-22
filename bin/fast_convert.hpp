3bf3ca3ff7679c46ff3020bfcbac4e77e4b9ff49
Francois A <francoisancel@gmail.com>
2016-12-22 17:08:29 +0100

    Introducing fast_convert files

---
+++ bin/fast_convert.hpp
@@ -0,0 +1,20 @@
+#ifndef FAST_CONVERT_HPP
+#define FAST_CONVERT_HPP
+
+#include "astring_view.hpp"
+
+namespace xts
+{
+	//Assume that the input string have only numeral, no space, no '-', no '+', and doesn't overflow
+	std::size_t fast_str_to_uint(astd::astring_view str)
+	{
+		std::size_t result = 0;
+		for (char c : str)
+		{
+			result = result * 10 + (c - '0');
+		}
+		return result;
+	}
+}
+
+#endif //FAST_CONVERT_HPP
